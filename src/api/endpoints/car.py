08d5cbea78e3410a0ff87bd65a464ceb283a5f4e
Marcelo Cesario Miguel <marcelocmiguel@outlook.com>
2020-05-17 16:04:05 -0300

    Feito o Update do carros

--- src/api/endpoints/car.py
+++ src/api/endpoints/car.py
@@ -9,3 +9,15 @@ from src.crud.utils import ExistenceException, NonExistenceException
 from src.schemas.car import CarCreate, CarInDB, CarUpdate
 
 router = APIRouter()
+
+
+@router.put("/", response_model=CarInDB)
+def update_car(*, db: Session = Depends(get_db), car_id: int, car_info: CarUpdate):
+    """
+    Updates the Car specified by id, with the body request fields.
+    """
+    try:
+        updated_car = update(db_session=db, car_id=car_id, obj_in=car_info)
+    except NonExistenceException as err:
+        raise HTTPException(status_code=303, detail=err.message)
+    return updated_car

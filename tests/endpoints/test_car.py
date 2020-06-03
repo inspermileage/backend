cffc2bd121639805d24e83d6907910f59fd66445
William Silva <williamars@al.insper.edu.br>
2020-06-03 20:38:16 -0300

    Tests read_car

--- tests/endpoints/test_car.py
+++ tests/endpoints/test_car.py
@@ -4,10 +4,25 @@ from main import app
 
 client = TestClient(app)
 
+def test_read_car_by_name():
+    insert_data = { "name": "Teste",
+                    "description":"Teste", 
+                    "creation_date": "2020-06-02"}
+    car_response=client.post("/api/car/", json=insert_data)
 
+    assert car_response.status_code == 200
+    read_name = car_response.json()["name"]
 
+    read_response = client.get(f"/api/car/{read_name}")
+    response_data = car_response.json()
+    assert read_response.status_code == 200
+    assert insert_data["description"] == response_data["description"]
+    assert insert_data["creation_date"] == response_data["creation_date"]
 
-
+def test_read_car():
+    read_response = client.get(f"/api/car/")
+    assert read_response.status_code == 200
+    assert type(read_response.json()) == list
     
 def test_delete_car():
     read_response = client.get(f"/api/car/")

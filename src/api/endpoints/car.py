1229a2351c09e96f740ec7d6d8b25d5379dc4d6c
nicole costa <nicolesarvasi@hotmail.com>
2020-07-15 21:07:23 -0300

    refatoracao car

--- src/api/endpoints/car.py
+++ src/api/endpoints/car.py
@@ -8,6 +8,7 @@ from src.api.utils.db import get_db
 from src.crud.utils import ExistenceException, NonExistenceException
 from src.schemas.car import CarCreate, CarInDB, CarUpdate
 from src.models.car import Car as CarModel
+from src.crud.crud_car import create, delete, read_all, read_one, update
 
 router = APIRouter()
 
@@ -18,14 +19,10 @@ def create_car(*, db: Session = Depends(get_db), car_in: CarCreate):
     Inserts a car into the car table, given the request body.
     """
     try:
-        obj_in_data = jsonable_encoder(car_in)
-        db_obj = CarModel(**obj_in_data)
-        db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
-        return db_obj
-    except Exception as err:
-        raise HTTPException(status_code=500, detail=err)
+        created_car = create(db_session=db, obj_in=car_in)
+    except ExistenceException as err:
+        raise HTTPException(status_code=404, detail="Car not found")
+    return created_car
 
 
 @router.get("/", response_model=List[CarInDB])
@@ -34,42 +31,31 @@ def read_car(db: Session = Depends(get_db)):
     Returns a list of all the car entries in the database.
     """
 
-    return db.query(CarModel).all()
+    return read_all(db_session=db)
 
 
 @router.get("/{name}", response_model=CarInDB)
-def read_car(*, db: Session = Depends(get_db), name: str):
+def read_car_by_name(*, db: Session = Depends(get_db), name: str):
     """
     Returns a Car specified by the name
     """
 
-    car_obj = db.query(CarModel).filter(CarModel.name == name).first()
-    if not car_obj:
-        raise HTTPException(status_code=404, detail="Car not found")
-    return car_obj
+    try:
+        existing_car = read_one(db_session=db, car_name=name)
+    raise HTTPException(status_code=404, detail="Car not found")
+    return existing_car
 
 
 @router.put("/{car_id}", response_model=CarInDB)
 def update_car(*, db: Session = Depends(get_db), car_id: int, car_info: CarUpdate):
     """
-    Updates the Car specified by the name field in the request body, with the rest of the body fields.
+    Updates the Car specified by the ID field in the request body, with the rest of the body fields.
     """
 
-    # Tries to find the car by name
-    car_obj = db.query(CarModel).filter(CarModel.id == car_id).first()
-    if not car_obj:
-        raise HTTPException(status_code=404, detail="Car not found")
-
-    # Iterates request body and updates the car object
-    obj_data = jsonable_encoder(car_obj)
-    update_data = car_info.dict(exclude_unset=True)
-    for field in obj_data:
-        if field in update_data:
-            setattr(car_obj, field, update_data[field])
-    db.add(car_obj)
-    db.commit()
-    db.refresh(car_obj)
-    return car_obj
+   try:
+        updated_car = update(db_session=db, car_id=car_id, obj_in=car_info)
+    raise HTTPException(status_code=404, detail="Car not found")
+    return updated_car
 
 
 @router.delete("/{name}", response_model=CarInDB)
@@ -78,10 +64,7 @@ def delete_car(*, db: Session = Depends(get_db), name: str):
     Deletes a Car specified by the name.
     """
 
-    car_obj = db.query(CarModel).filter(CarModel.name == name).first()
-    if not car_obj:
-        raise HTTPException(status_code=404, detail="Car not found")
-
-    db.delete(car_obj)
-    db.commit()
-    return car_obj
+    try:
+        deleted_car = delete(db_session=db, car_name=name)
+    raise HTTPException(status_code=404, detail="Car not found")
+    return deleted_car

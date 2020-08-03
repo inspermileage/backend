93766a6d83768e3ca8987c4f1faf8c9c0a597a26
Beatriz Rianho Bernadinho <pbeatriz18@hotmail.com.br>
2020-08-03 19:48:35 -0300

    modificacoes

--- tests/endpoints/test_telemetry.py
+++ tests/endpoints/test_telemetry.py
@@ -91,50 +91,50 @@ def test_create_telemetry():
 
 
 
-# def test_read_telemetry_by_id():
-#     track_response = client.post("/api/track/", json={"name": random_lower_string(), "description": random_lower_string()})
-#     track_id = track_response.json()["id"]
+def test_read_telemetry_by_id():
+    track_response = client.post("/api/track/", json={"name": random_lower_string(), "description": random_lower_string()})
+    track_id = track_response.json()["id"]
 
-#     car_response=client.post("/api/car/", json={ "name": random_lower_string(), "description":random_lower_string(), "creation_date": "2020-06-02"})
-#     car_id=car_response.json()["id"]
+    car_response=client.post("/api/car/", json={ "name": random_lower_string(), "description":random_lower_string(), "creation_date": "2020-06-02"})
+    car_id=car_response.json()["id"]
 
-#     # Creates a round so tests dont give a FK error on inserting round
-#     round_response = client.post("/api/round/", json={
-#     "name": random_lower_string(),
-#     "description": random_lower_string(),
-#     "reason": "Test",
-#     "ref_date": "2020-07-26",
-#     "track_id": track_id,
-#     "car_id": car_id
-#     })
-#     round_id = round_response.json()["id"]
+    # Creates a round so tests dont give a FK error on inserting round
+    round_response = client.post("/api/round/", json={
+    "name": random_lower_string(),
+    "description": random_lower_string(),
+    "reason": "Test",
+    "ref_date": "2020-07-26",
+    "track_id": track_id,
+    "car_id": car_id
+    })
+    round_id = round_response.json()["id"]
 
-#     insert_data = {
-#             "speed": 0,
-#             "distance": 0,
-#             "engine_temp": 0,
-#             "creation_time": "2020-07-26T18:14:17.378Z",
-#             "energy_cons": 0,
-#             "rpm": 0,
-#             "battery": 0,
-#             "round_id": round_id
-#             }
-#     telemetry_response=client.post("/api/telemetry/", json=insert_data)
+    insert_data = {
+            "speed": 0,
+            "distance": 0,
+            "engine_temp": 0,
+            "creation_time": "2020-07-26T18:14:17.378Z",
+            "energy_cons": 0,
+            "rpm": 0,
+            "battery": 0,
+            "round_id": round_id
+            }
+    telemetry_response=client.post("/api/telemetry/", json=insert_data)
 
-#     assert telemetry_response.status_code == 200
-#     read_id = telemetry_response.json()["id"]
+    assert telemetry_response.status_code == 200
+    read_id = telemetry_response.json()["id"]
 
-#     read_response = client.get(f"/api/telemetry/{read_id}")
-#     response_data = read_response.json()
-#     assert read_response.status_code == 200
-#     assert insert_data["speed"] == response_data["speed"]
-#     assert insert_data["distance"] == response_data["distance"]
-#     assert insert_data["engine_temp"] == response_data["engine_temp"]
-#     assert insert_data["creation_time"] == response_data["creation_time"]
-#     assert insert_data["energy_cons"] == response_data["energy_cons"]
-#     assert insert_data["rpm"] == response_data["rpm"]
-#     assert insert_data["battery"] == response_data["battery"]
-#     assert insert_data["round_id"] == response_data["round_id"]
+    read_response = client.get(f"/api/telemetry/{read_id}")
+    response_data = read_response.json()
+    assert read_response.status_code == 200
+    assert insert_data["speed"] == response_data["speed"]
+    assert insert_data["distance"] == response_data["distance"]
+    assert insert_data["engine_temp"] == response_data["engine_temp"]
+    assert insert_data["creation_time"] == response_data["creation_time"]
+    assert insert_data["energy_cons"] == response_data["energy_cons"]
+    assert insert_data["rpm"] == response_data["rpm"]
+    assert insert_data["battery"] == response_data["battery"]
+    assert insert_data["round_id"] == response_data["round_id"]
 
 
 
@@ -145,14 +145,14 @@ def test_read_telemetry():
     assert read_response.status_code == 200
     assert type(read_response.json()) == list
 
-# def test_read_invalid_telemetry():
+def test_read_invalid_telemetry():
 
-#     update_response = client.get(f"/api/telemetry/{0}")
-#     assert update_response.status_code == 404
+    update_response = client.get(f"/api/telemetry/{0}")
+    assert update_response.status_code == 404
 
-# def test_read_invalid_telemetry_two():
-#     update_response = client.get(f"/api/telemetryy/", headers={"id": 0})
-#     assert update_response.status_code == 404
+def test_read_invalid_telemetry_two():
+    update_response = client.get(f"/api/telemetryy/", headers={"id": 0})
+    assert update_response.status_code == 404
 
 
 def test_delete_telemetry():

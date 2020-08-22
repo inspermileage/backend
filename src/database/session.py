e4d39c6a614b866a37dfb0ffdcf5c51a1abdd3b6
Marcelo Cesario Miguel <marcelocmiguel@outlook.com>
2020-08-22 18:56:28 -0300

    Database_URI

--- src/database/session.py
+++ src/database/session.py
@@ -4,7 +4,7 @@ from sqlalchemy_utils import create_database, database_exists
 
 from src.core import config
 
-engine = create_engine(config.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+engine = create_engine(config.DATABASE_URL, pool_pre_ping=True)
 if not database_exists(engine.url):
     create_database(engine.url)
 

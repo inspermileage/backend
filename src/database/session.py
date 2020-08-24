767f51cc58630520be7fad67e1bf11f223634ea3
Beatriz Rianho Bernadinho <pbeatriz18@hotmail.com.br>
2020-08-24 19:21:43 -0300

    engine

--- src/database/session.py
+++ src/database/session.py
@@ -5,7 +5,7 @@ from sqlalchemy_utils import create_database, database_exists
 from src.core import config
 
 engine = create_engine(config.DATABASE_URL, pool_pre_ping=True)
-if not database_exists(engine.url):
-    create_database(engine.url)
+# if not database_exists(engine.url):
+#     create_database(engine.url)
 
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
